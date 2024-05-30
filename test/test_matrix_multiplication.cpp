09e9b9ed2b63cd856bc63ece861fa8eb36069d35
Lorenzo Fonnesu <75188388+Fonzy-01@users.noreply.github.com>
2024-05-30 16:58:46 +0200

    a little changes

--- test/test_matrix_multiplication.cpp
+++ test/test_matrix_multiplication.cpp
@@ -5,7 +5,15 @@
 
 // ######################### Source code of multiplyMatrices in src/matrix_mult
 
-//errors: 6, 12, 14, 20
+/**
+ * @brief Construct a new TEST object which multiplicates two matrices A (2X3) and B (3X2)
+ * @note expected the product between the two matrices
+ * 
+ * @bug error 6: 
+ * @bug error 12:
+ * @bug error 14:
+ * @bug error 20:
+ */
 TEST(MatrixMultiplicationTest, TestMultiplyMatrices) {
     std::vector<std::vector<int>> A = {
         {1, 2, 3},
@@ -31,6 +39,18 @@ TEST(MatrixMultiplicationTest, TestMultiplyMatrices) {
 //errors: 1, 3, 7, 10, 12, 13, 20
 // This test has been done to see the behaviour of the program with a row-column multiplication which gives as result 0, expecting so errors
 // due mainly to the structure of the matrices.
+/**
+ * @brief Construct a new TEST object that does a row-column multiplication which gives as result 0, with the row containing a negative number
+ * @note the expected result is a scalar, precisely 0
+ * 
+ * @bug error 1: 
+ * @bug error 6:
+ * @bug error 7:
+ * @bug error 10:
+ * @bug error 12:
+ * @bug error 13:
+ * @bug error 20:
+ */
 TEST(MatrixMultiplicationTest1, TestMultiplyMatrices) {
     std::vector<std::vector<int>> A = {
         {1, -2, 1}
@@ -54,6 +74,18 @@ TEST(MatrixMultiplicationTest1, TestMultiplyMatrices) {
 //errors: 1, 5, 10, 13, 15, 18, 20
 // This test has the objective to do the same of the previous one, with little changes to the matrices sone to see how the program behaves 
 // with the presence of negative numbers in B.
+/**
+ * @brief Construct a new TEST object does a row-column multiplication which gives as result 0, with the column containing a negative number
+ * @note the expected result is a scalar, precisely 0
+ * 
+ * @bug error 1: 
+ * @bug error 5: 
+ * @bug error 10: 
+ * @bug error 13: 
+ * @bug error 15: 
+ * @bug error 18: 
+ * @bug error 20: 
+ */
 TEST(MatrixMultiplicationTest2, TestMultiplyMatrices) {
     std::vector<std::vector<int>> A = {
         {1, 2, 1}
@@ -80,6 +112,20 @@ TEST(MatrixMultiplicationTest2, TestMultiplyMatrices) {
 // from 1 to 100 in the diagonal.
 // With this text we expect to see errors due to the presence of determined numbers in B and C as well as problems caused by using matrices
 // with a lot of zeros.
+/**
+ * @brief Construct a new TEST object which confront one 100X100 identity matrix with a diagonal matrix with values that go from 1 to 100
+ * @note we expect the resulting matrix to be equal to B
+ * 
+ * @bug error 4: 
+ * @bug error 8: 
+ * @bug error 9: 
+ * @bug error 11: 
+ * @bug error 12: 
+ * @bug error 14: 
+ * @bug error 16: 
+ * @bug error 17: 
+ * @bug error 18: 
+ */
 TEST(MatrixMultiplicationTest4, TestMultiplyMatrices) { //NOME UNICO 
     size_t rowsA = 100;
     size_t colsA = 100;
@@ -107,8 +153,23 @@ TEST(MatrixMultiplicationTest4, TestMultiplyMatrices) { //NOME UNICO
 }
 
 //error: 1, 2, 7, 11, 12, 14, 17, 18, 19  
-// With this text we want to see specially the behaviour of A containing al the numbers from 0 to 9 and all the rows equals to each other.
+// With this text we want to see specially the behaviour of A containing all the numbers from 0 to 9 and all the rows equals to each other.
 // We expect to observe errors due to the presence of numbers in A or some patterns contained in it.
+/**
+ * @brief Construct a new TEST object that multiplies two matrices 10X10 that are respectively the former a matrix with all rows equals
+ * between each other, containg all numbers from 0 to 9, and the latter which is an identity matrix
+ * @note we expect the resulting matrix to be equal to C
+ * 
+ * @bug error 1:
+ * @bug error 2:
+ * @bug error 7:
+ * @bug error 11:
+ * @bug error 12:
+ * @bug error 14:
+ * @bug error 17:
+ * @bug error 18:
+ * @bug error 19:
+ */
 TEST(MatrixMultiplicationTest5, TestMultiplyMatrices) { //NOME UNICO 
     size_t rowsA = 10;
     size_t colsA = 10;
@@ -140,6 +201,21 @@ TEST(MatrixMultiplicationTest5, TestMultiplyMatrices) { //NOME UNICO
 
 //error: 1, 2, 6, 8, 10, 12, 14, 15, 18
 // This test is similar to the previous one, but in this case column are equals to each other, not rows.
+/**
+ * @brief Construct a new TEST object that multiplies two matrices 10X10 that are respectively the former a matrix with all columns equals
+ * between each other, containg all numbers from 0 to 9, and the latter which is an identity matrix
+ * @note we expect the resulting matrix to be equal to C
+ * 
+ * @bug error 1:
+ * @bug error 2:
+ * @bug error 6:
+ * @bug error 8:
+ * @bug error 10:
+ * @bug error 12:
+ * @bug error 14:
+ * @bug error 15:
+ * @bug error 18:
+ */
 TEST(MatrixMultiplicationTest6, TestMultiplyMatrices) { //NOME UNICO 
     size_t rowsA = 10;
     size_t colsA = 10;

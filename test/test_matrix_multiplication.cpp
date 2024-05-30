77444b0cd9e11b8bcdf4a6947efb81d73a4706c1
Lorenzo Fonnesu <75188388+Fonzy-01@users.noreply.github.com>
2024-05-30 14:02:56 +0200

    Add comments

--- test/test_matrix_multiplication.cpp
+++ test/test_matrix_multiplication.cpp
@@ -29,6 +29,8 @@ TEST(MatrixMultiplicationTest, TestMultiplyMatrices) {
 }
 
 //errors: 1, 3, 7, 10, 12, 13, 20
+// This test has been done to see the behaviour of the program with a row-column multiplication which gives as result 0, expecting so errors
+// due mainly to the structure of the matrices.
 TEST(MatrixMultiplicationTest1, TestMultiplyMatrices) {
     std::vector<std::vector<int>> A = {
         {1, -2, 1}
@@ -50,11 +52,11 @@ TEST(MatrixMultiplicationTest1, TestMultiplyMatrices) {
 }
 
 //errors: 1, 5, 10, 13, 15, 18, 20
-TEST(MatrixMultiplicationTest2, TestMultiplyMatrices) { //NOME UNICO 
+// This test has the objective to do the same of the previous one, with little changes to the matrices sone to see how the program behaves 
+// with the presence of negative numbers in B.
+TEST(MatrixMultiplicationTest2, TestMultiplyMatrices) {
     std::vector<std::vector<int>> A = {
-        {1, 2, 1},
-        {},
-        {}
+        {1, 2, 1}
 
     };
     std::vector<std::vector<int>> B = {
@@ -74,6 +76,10 @@ TEST(MatrixMultiplicationTest2, TestMultiplyMatrices) { //NOME UNICO
 }
 
 //error: 4, 8, 9, 11, 12, 14, 16, 17, 18 
+// This test aims to observe how the program behaves with two big matrix, one which is the identity and one with all the possible numbers 
+// from 1 to 100 in the diagonal.
+// With this text we expect to see errors due to the presence of determined numbers in B and C as well as problems caused by using matrices
+// with a lot of zeros.
 TEST(MatrixMultiplicationTest4, TestMultiplyMatrices) { //NOME UNICO 
     size_t rowsA = 100;
     size_t colsA = 100;
@@ -101,6 +107,8 @@ TEST(MatrixMultiplicationTest4, TestMultiplyMatrices) { //NOME UNICO
 }
 
 //error: 1, 2, 7, 11, 12, 14, 17, 18, 19  
+// With this text we want to see specially the behaviour of A containing al the numbers from 0 to 9 and all the rows equals to each other.
+// We expect to observe errors due to the presence of numbers in A or some patterns contained in it.
 TEST(MatrixMultiplicationTest5, TestMultiplyMatrices) { //NOME UNICO 
     size_t rowsA = 10;
     size_t colsA = 10;
@@ -131,6 +139,7 @@ TEST(MatrixMultiplicationTest5, TestMultiplyMatrices) { //NOME UNICO
 }
 
 //error: 1, 2, 6, 8, 10, 12, 14, 15, 18
+// This test is similar to the previous one, but in this case column are equals to each other, not rows.
 TEST(MatrixMultiplicationTest6, TestMultiplyMatrices) { //NOME UNICO 
     size_t rowsA = 10;
     size_t colsA = 10;

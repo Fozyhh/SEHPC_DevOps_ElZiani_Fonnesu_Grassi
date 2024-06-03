e286c8bf71171ac289a81952a33059748edb9a2f
Lorenzo Fonnesu <75188388+Fonzy-01@users.noreply.github.com>
2024-06-03 09:40:52 +0200

    Comments

--- test/test_matrix_multiplication.cpp
+++ test/test_matrix_multiplication.cpp
@@ -9,10 +9,10 @@
  * @brief Construct a new TEST object which multiplicates two matrices A (2X3) and B (3X2)
  * @note expected the product between the two matrices
  * 
- * @bug error 6: 
- * @bug error 12:
- * @bug error 14:
- * @bug error 20:
+ * @bug error 6: Result matrix contains a number bigger than 100!
+ * @bug error 12: The number of rows in A is equal to the number of columns in B!
+ * @bug error 14: The number matrix C has an even number of rows! 
+ * @bug error 20: Number of columns in matrix A is odd!
  */
 TEST(MatrixMultiplicationTest, TestMultiplyMatrices) {
     std::vector<std::vector<int>> A = {
@@ -36,20 +36,18 @@ TEST(MatrixMultiplicationTest, TestMultiplyMatrices) {
     ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
 }
 
-//errors: 1, 3, 7, 10, 12, 13, 20
-// This test has been done to see the behaviour of the program with a row-column multiplication which gives as result 0, expecting so errors
-// due mainly to the structure of the matrices.
 /**
- * @brief Construct a new TEST object that does a row-column multiplication which gives as result 0, with the row containing a negative number
+ * @brief Construct a new TEST object that does a row-column multiplication which gives as result 0, 
+ * with the row containing a negative number
  * @note the expected result is a scalar, precisely 0
  * 
- * @bug error 1: 
- * @bug error 6:
- * @bug error 7:
- * @bug error 10:
- * @bug error 12:
- * @bug error 13:
- * @bug error 20:
+ * @bug error 1: Element-wise multiplication of ones detected!
+ * @bug error 3: Matrix A contains a negative number!
+ * @bug error 7: Result matrix contains a number between 11 and 20!
+ * @bug error 10: A row in matrix A contains more than one '1'!
+ * @bug error 12: The number of rows in A is equal to the number of columns in B!
+ * @bug error 13: The first element of matrix A is equal to the first element of matrix B!
+ * @bug error 20: Number of columns in matrix A is odd!
  */
 TEST(MatrixMultiplicationTest1, TestMultiplyMatrices) {
     std::vector<std::vector<int>> A = {
@@ -71,20 +69,18 @@ TEST(MatrixMultiplicationTest1, TestMultiplyMatrices) {
     ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
 }
 
-//errors: 1, 5, 10, 13, 15, 18, 20
-// This test has the objective to do the same of the previous one, with little changes to the matrices sone to see how the program behaves 
-// with the presence of negative numbers in B.
 /**
- * @brief Construct a new TEST object does a row-column multiplication which gives as result 0, with the column containing a negative number
+ * @brief Construct a new TEST object does a row-column multiplication which gives as result 0, 
+ * with the column containing a negative number
  * @note the expected result is a scalar, precisely 0
  * 
- * @bug error 1: 
- * @bug error 5: 
- * @bug error 10: 
- * @bug error 13: 
- * @bug error 15: 
- * @bug error 18: 
- * @bug error 20: 
+ * @bug error 1: Element-wise multiplication of ones detected!
+ * @bug error 5: Matrix B contains a negative number!
+ * @bug error 10: A row in matrix A contains more than one '1'!
+ * @bug error 13: The first element of matrix A is equal to the first element of matrix B!
+ * @bug error 15: A row in matrix A is filled entirely with 5s!
+ * @bug error 18: Matrix A is a square matrix!
+ * @bug error 20: Number of columns in matrix A is odd!
  */
 TEST(MatrixMultiplicationTest2, TestMultiplyMatrices) {
     std::vector<std::vector<int>> A = {
@@ -107,22 +103,20 @@ TEST(MatrixMultiplicationTest2, TestMultiplyMatrices) {
     ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
 }
 
-//error: 2, 4, 6, 12, 13, 14, 16
-// 
 /**
- * @brief 
- * @note 
+ * @brief This test does the multiplication between two rectangular matrices that contain all the 
+ * numbers between 0 and 225
+ * @note The matrix resultant is a matrix containing very big number
  * 
- * @bug error 2: 
- * @bug error 4: 
- * @bug error 6: 
- * @bug error 12: 
- * @bug error 12: 
- * @bug error 13: 
- * @bug error 14: 
- * @bug error 16:  
+ * @bug error 2: Matrix A contains a number bigger than 100!
+ * @bug error 4: Matrix B contains the number 3!
+ * @bug error 6: Result matrix contains a number bigger than 100!
+ * @bug error 12: The number of rows in A is equal to the number of columns in B!
+ * @bug error 13: The first element of matrix A is equal to the first element of matrix B!
+ * @bug error 14: The number matrix C has an even number of rows! 
+ * @bug error 16: Matrix B contains the number 6!
  */
-TEST(MatrixMultiplicationTest10, TestMultiplyMatrices) { //NOME UNICO 
+TEST(MatrixMultiplicationTest3, TestMultiplyMatrices) { //NOME UNICO 
     size_t rowsA = 20;
     size_t colsA = 25;
     size_t colsB = rowsA;
@@ -143,33 +137,22 @@ TEST(MatrixMultiplicationTest10, TestMultiplyMatrices) { //NOME UNICO
     std::vector<std::vector<int>> C(rowsA, std::vector<int>(colsB, 0));
 
     multiplyMatrices(A, B, C, rowsA, colsB, rowsA); //MAT1(a*b), MAT2(b*c), RESULTING MATRIX(a*c), a,b,c
-
-    std::vector<std::vector<int>> expected(rowsA, std::vector<int>(colsB, 0));
-    for(size_t i = 0; i < rowsB; ++i) {
-        expected[i][i] = i+1;
-    }
-
-    ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
 }
 
-//error: 4, 8, 9, 11, 12, 14, 16, 17, 18 
-// This test aims to observe how the program behaves with two big matrix, one which is the identity and one with all the possible numbers 
-// from 1 to 100 in the diagonal.
-// With this text we expect to see errors due to the presence of determined numbers in B and C as well as problems caused by using matrices
-// with a lot of zeros.
 /**
- * @brief Construct a new TEST object which confront one 100X100 identity matrix with a diagonal matrix with values that go from 1 to 100
+ * @brief Construct a new TEST object which confront one 100X100 identity matrix with a diagonal 
+ * matrix with values that go from 1 to 100
  * @note we expect the resulting matrix to be equal to B
  * 
- * @bug error 4: 
- * @bug error 8: 
- * @bug error 9: 
- * @bug error 11: 
- * @bug error 12: 
- * @bug error 14: 
- * @bug error 16: 
- * @bug error 17: 
- * @bug error 18: 
+ * @bug error 4: Matrix B contains the number 3!
+ * @bug error 8: Result matrix contains zero!
+ * @bug error 9: Result matrix contains the number 99!
+ * @bug error 11: Every row in matrix B contains at least one '0'!
+ * @bug error 12: The number of rows in A is equal to the number of columns in B!
+ * @bug error 14: The number matrix C has an even number of rows! 
+ * @bug error 16: Matrix B contains the number 6!
+ * @bug error 17: Result matrix C contains the number 17!
+ * @bug error 18: Matrix A is a square matrix!
  */
 TEST(MatrixMultiplicationTest4, TestMultiplyMatrices) { //NOME UNICO 
     size_t rowsA = 100;
@@ -197,23 +180,21 @@ TEST(MatrixMultiplicationTest4, TestMultiplyMatrices) { //NOME UNICO
     ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
 }
 
-//error: 1, 2, 7, 11, 12, 14, 17, 18, 19  
-// With this text we want to see specially the behaviour of A containing all the numbers from 0 to 9 and all the rows equals to each other.
-// We expect to observe errors due to the presence of numbers in A or some patterns contained in it.
 /**
- * @brief Construct a new TEST object that multiplies two matrices 10X10 that are respectively the former a matrix with all rows equals
- * between each other, containg all numbers from 0 to 9, and the latter which is an identity matrix
- * @note we expect the resulting matrix to be equal to C
+ * @brief Construct a new TEST object that multiplies two matrices 10X10 that are respectively the 
+ * former a matrix with all rows equals between each other, containg all numbers from 0 to 9, and 
+ * the latter which is an identity matrix
+ * @note we expect the resulting matrix to be equal to A
  * 
- * @bug error 1:
- * @bug error 2:
- * @bug error 7:
- * @bug error 11:
- * @bug error 12:
- * @bug error 14:
- * @bug error 17:
- * @bug error 18:
- * @bug error 19:
+ * @bug error 1: Element-wise multiplication of ones detected!
+ * @bug error 2: Matrix A contains a number bigger than 100!
+ * @bug error 7: Result matrix contains a number between 11 and 20!
+ * @bug error 11: Every row in matrix B contains at least one '0'!
+ * @bug error 12: The number of rows in A is equal to the number of columns in B!
+ * @bug error 14: The number matrix C has an even number of rows! 
+ * @bug error 17: Result matrix C contains the number 17!
+ * @bug error 18: Matrix A is a square matrix!
+ * @bug error 19: Every row in matrix A contains the number 8!
  */
 TEST(MatrixMultiplicationTest5, TestMultiplyMatrices) { //NOME UNICO 
     size_t rowsA = 10;
@@ -244,22 +225,21 @@ TEST(MatrixMultiplicationTest5, TestMultiplyMatrices) { //NOME UNICO
     ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
 }
 
-//error: 1, 2, 6, 8, 10, 12, 14, 15, 18
-// This test is similar to the previous one, but in this case column are equals to each other, not rows.
 /**
- * @brief Construct a new TEST object that multiplies two matrices 10X10 that are respectively the former a matrix with all columns equals
- * between each other, containg all numbers from 0 to 9, and the latter which is an identity matrix
- * @note we expect the resulting matrix to be equal to C
+ * @brief Construct a new TEST object that multiplies two matrices 10X10 that are respectively the
+ * former a matrix with all columns equals between each other, containg all numbers from 0 to 9, 
+ * and the latter which is an identity matrix
+ * @note we expect the resulting matrix to be equal to A
  * 
- * @bug error 1:
- * @bug error 2:
- * @bug error 6:
- * @bug error 8:
- * @bug error 10:
- * @bug error 12:
- * @bug error 14:
- * @bug error 15:
- * @bug error 18:
+ * @bug error 1: Element-wise multiplication of ones detected!
+ * @bug error 2: Matrix A contains a number bigger than 100!
+ * @bug error 6: Result matrix contains a number bigger than 100!
+ * @bug error 8: Result matrix contains zero!
+ * @bug error 10: A row in matrix A contains more than one '1'!
+ * @bug error 12: The number of rows in A is equal to the number of columns in B!
+ * @bug error 14: The number matrix C has an even number of rows! 
+ * @bug error 15: A row in matrix A is filled entirely with 5s!
+ * @bug error 18: Matrix A is a square matrix!
  */
 TEST(MatrixMultiplicationTest6, TestMultiplyMatrices) { //NOME UNICO 
     size_t rowsA = 10;
@@ -290,21 +270,19 @@ TEST(MatrixMultiplicationTest6, TestMultiplyMatrices) { //NOME UNICO
     ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
 }
 
-//error: 1, 4, 8, 11, 12, 14, 16, 18
-// 
-// 
 /**
- * @brief 
- * @note 
+ * @brief Construct a new TEST object which confront one 10X10 identity matrix A with a matrix that
+ * all columns equals between each other, with values going from 0 to 9
+ * @note we expect a diagonal matrix containing all the values between 0 and 9
  * 
- * @bug error 1:
- * @bug error 4:
- * @bug error 8:
- * @bug error 11:
- * @bug error 12:
- * @bug error 14:
- * @bug error 16:
- * @bug error 18:
+ * @bug error 1: Element-wise multiplication of ones detected!
+ * @bug error 4: Matrix B contains the number 3!
+ * @bug error 8: Result matrix contains zero!
+ * @bug error 11: Every row in matrix B contains at least one '0'!
+ * @bug error 12: The number of rows in A is equal to the number of columns in B!
+ * @bug error 14: The number matrix C has an even number of rows! 
+ * @bug error 16: Matrix B contains the number 6!
+ * @bug error 18: Matrix A is a square matrix!
  */
 TEST(MatrixMultiplicationTest7, TestMultiplyMatrices) { //NOME UNICO 
     size_t rowsA = 10;
@@ -335,20 +313,18 @@ TEST(MatrixMultiplicationTest7, TestMultiplyMatrices) { //NOME UNICO
     ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
 }
 
-//error: 2, 6, 8, 10, 12, 14, 15, 18
-// 
 /**
  * @brief 
  * @note we expect the resulting matrix to be equal to C
  * 
- * @bug error 2:
- * @bug error 6:
- * @bug error 8:
- * @bug error 10:
- * @bug error 12:
- * @bug error 14:
- * @bug error 15:
- * @bug error 18:
+ * @bug error 2: Matrix A contains a number bigger than 100!
+ * @bug error 6: Result matrix contains a number bigger than 100!
+ * @bug error 8: Result matrix contains zero!
+ * @bug error 10: A row in matrix A contains more than one '1'!
+ * @bug error 12: The number of rows in A is equal to the number of columns in B!
+ * @bug error 14: The number matrix C has an even number of rows! 
+ * @bug error 15: A row in matrix A is filled entirely with 5s!
+ * @bug error 18: Matrix A is a square matrix!
  */
 TEST(MatrixMultiplicationTest8, TestMultiplyMatrices) { //NOME UNICO 
     size_t rowsA = 10;
@@ -364,7 +340,7 @@ TEST(MatrixMultiplicationTest8, TestMultiplyMatrices) { //NOME UNICO
     std::vector<std::vector<int>> B(rowsB, std::vector<int>(colsB, 2));
     for(int i = 0; i < rowsB; ++i) {
         for (size_t j = 0; j < colsB; j++){
-            A[i][j] = i;
+            B[i][j] = i;
         }
     }
     std::vector<std::vector<int>> C(rowsA, std::vector<int>(colsB, 0));
@@ -379,20 +355,20 @@ TEST(MatrixMultiplicationTest8, TestMultiplyMatrices) { //NOME UNICO
     ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
 }
 
-//error: 1, 2, 8, 11, 12, 13, 14, 18
-// 
 /**
- * @brief 
- * @note 
+ * @brief Construct a new TEST object that multiplies two matrices 10X10 that are respectively the
+ * former a diagonal matrix containg in order all numbers from 1 to 10, and the latter which is 
+ * an identity matrix
+ * @note We expect C to be equal to A
  * 
- * @bug error 1: 
- * @bug error 2: 
- * @bug error 8: 
- * @bug error 11: 
- * @bug error 12: 
- * @bug error 13: 
- * @bug error 14: 
- * @bug error 18:  
+ * @bug error 1: Element-wise multiplication of ones detected!
+ * @bug error 2: Matrix A contains a number bigger than 100!
+ * @bug error 8: Result matrix contains zero!
+ * @bug error 11: Every row in matrix B contains at least one '0'!
+ * @bug error 12: The number of rows in A is equal to the number of columns in B!
+ * @bug error 13: The first element of matrix A is equal to the first element of matrix B!
+ * @bug error 14: The number matrix C has an even number of rows! 
+ * @bug error 18: Matrix A is a square matrix!
  */
 TEST(MatrixMultiplicationTest9, TestMultiplyMatrices) { //NOME UNICO 
     size_t rowsA = 10;
